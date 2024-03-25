7a3de05d02681476889972011f5e7b8f89e91bc4
Dominik Kalicun <kalicundominik@gmail.com>
2024-03-25 21:59:22 +0100

    idk.

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import machine, time
 
-# wlan = network.WLAN(network.STA_IF)
-# wlan.active(True)
-
-# if not wlan.isconnected():
-#     wlan.connect(dotenv.WIFI, dotenv.PASSWORD)
-
-#     while not wlan.isconnected():
-#         time.sleep(1)
-#         pass
-
-# print(wlan.ifconfig())
-
-# sensor1 = machine.ADC(machine.Pin(34))
+sensor1 = machine.ADC(machine.Pin(34))
 sensor2 = machine.ADC(machine.Pin(39))
 sensor3 = machine.ADC(machine.Pin(36))
 sensor4 = machine.ADC(machine.Pin(35))
 sensor5 = machine.ADC(machine.Pin(32))
-# sensor6 = machine.ADC(machine.Pin(33))
+sensor6 = machine.ADC(machine.Pin(33))
 
 right = machine.Pin(27, machine.Pin.OUT)
 rightBack = machine.Pin(13, machine.Pin.OUT)
@@ -32,6 +20,10 @@ lastSensor = -1
 
 # engineSpeed1 = machine.DAC(machine.Pin(25))
 # engineSpeed2 = machine.DAC(machine.Pin(26))
+
+# engineSpeed1.write(178)
+# engineSpeed2.write(178)
+
 
 def goStaight():
 	left.on()
@@ -60,43 +52,32 @@ def findLine():
 while True:
 
 	sensor=[
-		# sensor1.read(), # > sensorTriggerValue,
+		sensor1.read() > sensorTriggerValue,
 		sensor2.read() > sensorTriggerValue,
 		sensor3.read() > sensorTriggerValue,
 		sensor4.read() > sensorTriggerValue,
 		sensor5.read() > sensorTriggerValue,
-		# sensor6.read() # > sensorTriggerValue
+		sensor6.read() > sensorTriggerValue
 	]
 
-	if sensor[0] and not sensor[1] and not sensor[2] and sensor[3]:       goStaight()
-	if sensor[0] and sensor[1] and not sensor[2] and sensor[3]:           goStaight()
-	if sensor[0] and not sensor[1] and sensor[2] and sensor[3]:           goStaight()
+	if sensor[0] and sensor[1] and sensor[2] and not sensor[3] and sensor[4] and sensor[5]: leftEngine()
+	elif sensor[0] and sensor[1] and sensor[2] and not sensor[3] and not sensor[4] and sensor[5]: leftEngine()
+	elif sensor[0] and sensor[1] and sensor[2] and not sensor[3] and not sensor[4] and not sensor[5]: leftEngine()
+	elif sensor[0] and sensor[1] and sensor[2] and sensor[3] and not sensor[4] and sensor[5]: leftEngine()
+	elif sensor[0] and sensor[1] and sensor[2] and sensor[3] and not sensor[4] and not sensor[5]: leftEngine()
+	elif sensor[0] and sensor[1] and sensor[2] and sensor[3] and sensor[4] and not sensor[5]: leftEngine()
 
-	elif sensor[0] and not sensor[1] and not sensor[2] and not sensor[3]: leftEngine()
-	elif sensor[0] and sensor[1] and not sensor[2] and not sensor[3]:     leftEngine()
-	elif sensor[0] and sensor[1] and sensor[2] and not sensor[3]:         leftEngine()
+	elif sensor[0] and sensor[1] and not sensor[2] and sensor[3] and sensor[4] and sensor[5]: rightEngine()
+	elif sensor[0] and not sensor[1] and not sensor[2] and sensor[3] and sensor[4] and sensor[5]: rightEngine()
+	elif not sensor[0] and not sensor[1] and not sensor[2] and sensor[3] and sensor[4] and sensor[5]: rightEngine()
+	elif sensor[0] and not sensor[1] and sensor[2] and sensor[3] and sensor[4] and sensor[5]: rightEngine()
+	elif not sensor[0] and not sensor[1] and sensor[2] and sensor[3] and sensor[4] and sensor[5]: rightEngine()
+	elif not sensor[0] and sensor[1] and sensor[2] and sensor[3] and sensor[4] and sensor[5]: rightEngine()
 
-	elif not sensor[0] and not sensor[1] and not sensor[2] and sensor[3]: rightEngine()
-	elif not sensor[0] and not sensor[1] and sensor[2] and sensor[3]:     rightEngine()
-	elif not sensor[0] and sensor[1] and sensor[2] and sensor[3]:         rightEngine()
+	elif sensor[0] and sensor[1] and not sensor[2] and not sensor[3] and sensor[4] and sensor[5]: goStaight()
+	# if       sensor[0] and     sensor[1] and not sensor[2] and     sensor[3] and     sensor[4] and     sensor[5]: goStaight()
+	# if       sensor[0] and     sensor[1] and     sensor[2] and not sensor[3] and     sensor[4] and     sensor[5]: goStaight()
+	elif sensor[0] and sensor[1] and sensor[2] and sensor[3] and sensor[4] and sensor[5]: findLine()
 
-	elif sensor[0] and sensor[1] and sensor[2] and sensor[3]:             findLine()
-
-	# print(sensor)
+	print(sensor)
 	# time.sleep(0.3)
-
-	# print(postData)
-	# data = (ujson.dumps(postData)).encode()
-	# headers = {'Content-Type': 'application/json'}
-
-	# try:
-	#     res = urequests.post(
-	#         "http://192.168.8.111:5173/api/linefollower",
-	#         headers=headers,
-	#         data=data)
-
-	#     res.close()
-	#     print("request send")
-
-	# except Exception as e:
-	#     print("Error:", e)
